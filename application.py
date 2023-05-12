e261cdaf15ac7341b7ef955c9621c127c60f9d73
Manuel Guzman <mg1196956@gmail.com>
2023-05-11 18:01:57 -0600

    XD

---
+++ application.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<html><body><h1>Hello Best Bike App!</h1></body></html>\n"
+
+if __name__ ==' __main__ ':
+    app.run(debug=True)
